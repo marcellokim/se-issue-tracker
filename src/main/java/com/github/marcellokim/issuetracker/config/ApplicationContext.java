d2ff44b199f418384d1f50419ae60b3451b3671b
Yoondong Kim <marcellokim@cau.ac.kr>
2026-05-21 18:43:17 +0900

    refactor: CLI 조립 책임을 composition root로 이동

--- src/main/java/com/github/marcellokim/issuetracker/config/ApplicationContext.java
+++ src/main/java/com/github/marcellokim/issuetracker/config/ApplicationContext.java
@@ -1,11 +1,6 @@
 package com.github.marcellokim.issuetracker.config;
 
-import com.github.marcellokim.issuetracker.persistence.DatabaseInitializer;
-import com.github.marcellokim.issuetracker.persistence.DatabaseEnvironment;
-import com.github.marcellokim.issuetracker.persistence.DriverManagerConnectionProvider;
-import com.github.marcellokim.issuetracker.persistence.jdbc.JdbcRepositoryFactory;
 import com.github.marcellokim.issuetracker.service.AuthenticationService;
-import com.github.marcellokim.issuetracker.service.DashboardSummaryService;
 import com.github.marcellokim.issuetracker.ui.DemoDashboardPresenter;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -16,20 +11,7 @@ public record ApplicationContext(
 ) {
 
     public static ApplicationContext fromEnvironment() throws IOException, SQLException {
-        DatabaseEnvironment environment = DatabaseEnvironment.fromSystem();
-        var connectionProvider = DriverManagerConnectionProvider.from(environment);
-        DatabaseInitializer.initialize(connectionProvider);
-
-        var repositories = new JdbcRepositoryFactory(connectionProvider);
-        return new ApplicationContext(
-                new AuthenticationService(repositories.users()),
-                new DemoDashboardPresenter(new DashboardSummaryService(
-                        repositories.projects(),
-                        repositories.issues(),
-                        repositories.statistics(),
-                        repositories.users()
-                ))
-        );
+        return new ApplicationBootstrap().startUiContext();
     }
 
 }
