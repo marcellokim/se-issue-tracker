d3f1c51ddcec38fccfdea07d60fa5376f17792c0
msh0678 <moonsh0678@gmail.com>
2026-05-21 11:43:13 +0900

    도메인 작성 위한 프리즈, 컨플릭트 머지됨

---
+++ src/main/java/com/github/marcellokim/issuetracker/service/IssueResult.java
@@ -0,0 +1,15 @@
+package com.github.marcellokim.issuetracker.service;
+
+import com.github.marcellokim.issuetracker.domain.IssueStatus;
+import com.github.marcellokim.issuetracker.domain.Priority;
+import com.github.marcellokim.issuetracker.domain.User;
+
+public record IssueResult(
+        String issueId,
+        IssueStatus status,
+        Priority priority,
+        String title,
+        String description,
+        User reporter
+) {
+}
