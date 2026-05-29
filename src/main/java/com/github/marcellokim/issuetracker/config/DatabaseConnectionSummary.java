c4771b5f528dc7ecb3b69719535a9e96bce20fe5
mckimR1972 <mirmirmirnar19@naver.com>
2026-05-30 00:29:44 +0900

    CLI 관련 서비스 + 폴더 + 테스트 삭제

--- src/main/java/com/github/marcellokim/issuetracker/config/DatabaseConnectionSummary.java
+++
@@ -1,9 +0,0 @@
-package com.github.marcellokim.issuetracker.config;
-
-public record DatabaseConnectionSummary(
-        String url,
-        String user,
-        String currentSchema,
-        String containerName
-) {
-}
